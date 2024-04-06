c8f39dbffd6178583649c2ef72b0430234404f25
mituki1234 <144220463+mituki1234@users.noreply.github.com>
2024-04-06 03:45:53 +0000

    comit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+let money = 0;
+let moneyps = 0;
 
+const btn = document.getElementById("click");
+let moneydisplay = document.getElementById("clickdisplay");
+
+function click(){
+    money += 1;
+    moneydisplay.innerText = money;
+}
+setInterval(click(),100);
